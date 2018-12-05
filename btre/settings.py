cdfca2e6ed6f4e674d7666775c2d80453bf29eb3
Matt Goff <matt@goff.us>
2018-12-04 19:15:35 -0800

    12-4

--- btre/settings.py
+++ btre/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'welcome22',
+        'HOST': 'localhost'
     }
 }
 
@@ -126,3 +129,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
 print("Static_root = {}   Static_IRL = {}   Staticfiles_dir = {}".format(STATIC_ROOT,STATIC_URL,STATICFILES_DIRS))
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
